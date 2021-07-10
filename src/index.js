781190d151a9fde1fa51d112248ddd9d4848862d
Fambegbe Olamileke <olamileke.f@gmail.com>
2021-07-10 13:35:10 +0100

    feat: added pwa support

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import AppContextProvider from "./contexts/AppContext";
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,3 +12,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// registering the service worker for pwas
+serviceWorker.register();
